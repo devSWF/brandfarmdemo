554561e4b993f7bb34d317ad180cd233eb438849
swpark <andy.park95@gmail.com>
2021-03-24 20:27:08 +0900

    [UPDATE] update fm plan screen

--- lib/fm_screens/plan/fm_plan_calendar_widget.dart
+++ lib/fm_screens/plan/fm_plan_calendar_widget.dart
@@ -1,5 +1,7 @@
+import 'package:BrandFarm/blocs/fm_plan/fm_plan_bloc.dart';
 import 'package:BrandFarm/widgets/fm_shared_widgets/fm_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FMPlanCalendarWidget extends StatefulWidget {
   @override
@@ -7,21 +9,20 @@ class FMPlanCalendarWidget extends StatefulWidget {
 }
 
 class _FMPlanCalendarWidgetState extends State<FMPlanCalendarWidget> {
+  FMPlanBloc _fmPlanBloc;
 
   @override
   void initState() {
     super.initState();
-
+    _fmPlanBloc = BlocProvider.of<FMPlanBloc>(context);
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FMCalendar(
-        // onTap: (){},
-
+      child: BlocProvider.value(
+        value: _fmPlanBloc,
+        child: FMCalendar(),
       ),
     );
   }
