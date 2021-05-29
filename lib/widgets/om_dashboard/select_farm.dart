fd93b38e6faace79e466458c7ceb6a94408e0bd1
sungwoonpark <andy.park95@gmail.com>
2021-05-29 12:54:21 +0900

    [UPDATE] add nav page to om home notifications

--- lib/widgets/om_dashboard/select_farm.dart
+++ lib/widgets/om_dashboard/select_farm.dart
@@ -26,74 +26,61 @@ class _SelectFarmState extends State<SelectFarm> {
     return BlocConsumer<OMHomeBloc, OMHomeState>(
       listener: (context, state) {},
       builder: (context, state) {
-        return (state.currentFarmIndex != null)
-            ? Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 66,
-                    width: 814,
-                    padding: EdgeInsets.fromLTRB(29, 17, 0, 17),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Row(
-                      children:
-                          List.generate(state.farmList.length ?? 1, (index) {
-                        return Row(
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                _omHomeBloc
-                                    .add(ChangeCurrFarmIndex(index: index));
-                              },
-                              child: Container(
-                                width: 170,
-                                height: 32,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(
-                                        width: (state.currentFarmIndex == index)
-                                            ? 3
-                                            : 1,
-                                        color: (state.currentFarmIndex == index)
-                                            ? Color(0xFF15B85B)
-                                            : Color(0x40000000))),
-                                child: Center(
-                                  child: Text(
-                                    '${state.farmList[index].name}',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyText2
-                                        .copyWith(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.normal,
-                                          color:
-                                              (state.currentFarmIndex == index)
-                                                  ? Color(0xFF15B85B)
-                                                  : Color(0x40000000),
-                                        ),
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 66,
+              width: 814,
+              padding: EdgeInsets.fromLTRB(29, 17, 0, 17),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
+              child: Row(
+                children: List.generate(state.farmList.length ?? 1, (index) {
+                  return Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          _omHomeBloc.add(ChangeCurrFarmIndex(index: index));
+                        },
+                        child: Container(
+                          width: 170,
+                          height: 32,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(
+                                  width:
+                                      (state.currentFarmIndex == index) ? 3 : 1,
+                                  color: (state.currentFarmIndex == index)
+                                      ? Color(0xFF15B85B)
+                                      : Color(0x40000000))),
+                          child: Center(
+                            child: Text(
+                              '${state.farmList[index].name}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText2
+                                  .copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                    color: (state.currentFarmIndex == index)
+                                        ? Color(0xFF15B85B)
+                                        : Color(0x40000000),
                                   ),
-                                ),
-                              ),
                             ),
-                            SizedBox(
-                              width: 24,
-                            )
-                          ],
-                        );
-                      }),
-                    ),
-                  ),
-                ],
-              )
-            : Row(
-                children: [
-                  Container(
-                      width: 814,
-                      child: Center(child: CircularProgressIndicator())),
-                ],
-              );
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 24,
+                      )
+                    ],
+                  );
+                }),
+              ),
+            ),
+          ],
+        );
       },
     );
   }
